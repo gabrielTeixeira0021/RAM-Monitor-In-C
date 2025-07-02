66b6fc525dd424343c1557f2ff61619541eb229c
hyperGT <charlesgabriel102@gmail.com>
2025-07-02 19:16:13 -0300

    Feat: Process Usage Display

--- ram_monitor.c
+++ ram_monitor.c
@@ -1,19 +1,22 @@
-// Default libs
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>	// Windows resourses API
+#include <windows.h>	
+#include <psapi.h>
 
-// TODO: show process usage info 
+#define MB_CONVERSION 1048576.0f
+
+// show process usage info 
+void processUsage();
 
 // show main memory info
 void printMemoryInfo();
 
 int main(){		
+	printf("Memory RAM monitor\n");
+	printf("--------------------\n");
 	
-	while(1){
+	while(1){	
 		system("cls");
-		printf("Memory RAM monitor\n");
-		printf("--------------------\n");
 		printMemoryInfo();
 		printf("Press Ctr+C to exit...\n");
 		Sleep(1000); // Update each 1 second				
@@ -21,13 +24,36 @@ int main(){
 	return 0;
 }
 
-void printMemoryInfo(){
+void processUsage(){
+	PROCESS_MEMORY_COUNTERS_EX pmc;
+	
+	if(!GetProcessMemoryInfo(GetCurrentProcess(), 
+	(PROCESS_MEMORY_COUNTERS*)&pmc, 
+	sizeof(pmc))) {
+		printf("ERROR getting process info!\n");
+		return;
+	}
+	
+	printf("Process Memory Usage:\n");
+	printf(" Working Set: %.2f MB\n", (float)pmc.WorkingSetSize / MB_CONVERSION);
+	printf(" Private Bytes: %.2f MB\n", (float)pmc.PrivateUsage / MB_CONVERSION);
+}
+
+void printMemoryInfo(){	
 	MEMORYSTATUSEX memInfo;
 	memInfo.dwLength = sizeof(MEMORYSTATUSEX);
-	GlobalMemoryStatusEx(&memInfo);
+	
+	if(!GlobalMemoryStatusEx(&memInfo)) {
+		printf("Error getting system memory info!\n");
+		return;
+	}
+	
 	
 	printf("\nTotal System Memory:\n");
-	printf(" Total Physical Memory: %.2f MB\n", (float)memInfo.ullTotalPhys / (1024 * 1024));
-	printf(" Physical Memory Available: %.2f MB\n", (float)memInfo.ullAvailPhys / (1024 * 1024));
+	printf(" Total Physical Memory: %.2f MB\n", (float)memInfo.ullTotalPhys / MB_CONVERSION);
+	printf(" Physical Memory Available: %.2f MB\n", (float)memInfo.ullAvailPhys / MB_CONVERSION);
 	printf(" Memory In Use: %lu%%\n", memInfo.dwMemoryLoad);
+	
+	// Shows process RAM memory usage
+	processUsage();
 }
